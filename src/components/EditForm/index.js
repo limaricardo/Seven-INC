066e8fa811706129d8a5e4546029ef5344088b68
Ricardo <limaricardop@gmail.com>
2022-09-05 02:06:33 -0300

    Favicon SVN

--- src/components/EditForm/index.js
+++ src/components/EditForm/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import FormComp from '../UI/Form';
 import schema from './schema';
+import { dataMock } from '../Mock';
 
 const EditForm = ({employee}) => {
-
+  console.log(employee)
+  console.log(dataMock)
   return (
     <div>
       <FormComp initialValues={employee} legend={"* - Preencher campo obrigatório."} title={"Editar informações do funcionário"} schema={schema} toastText={"Informação de funcionário alterada com sucesso!"}>Editar</FormComp>
